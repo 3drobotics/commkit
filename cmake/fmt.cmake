4911383e250b162338b8ed51b2ca8e352fcbddb9
Liam Staskawicz <liam@stask.net>
2016-04-20 17:14:35 -0700

    fmt: ensure working directory is correct when invoked within a super project

--- cmake/fmt.cmake
+++ cmake/fmt.cmake
@@ -10,11 +10,11 @@ find_package(PythonInterp)
 # apply formatting
 add_custom_target(fmt
     COMMAND ${PYTHON_EXECUTABLE} tools/clang-format-run.py --apply
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    SOURCES ${CMAKE_SOURCE_DIR}/.clang-format)
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/.clang-format)
 
 # verify formatting
 add_custom_target(fmt-diff
     COMMAND ${PYTHON_EXECUTABLE} tools/clang-format-run.py
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    SOURCES ${CMAKE_SOURCE_DIR}/.clang-format)
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/.clang-format)
