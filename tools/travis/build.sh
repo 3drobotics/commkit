53c20aab683b5fe234e431456a1ae8b411efaf07
Liam Staskawicz <liam@stask.net>
2016-04-25 09:10:04 -0700

    travis: smaller build matrix & quicker capnp build & run tests

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -7,6 +7,9 @@ cd build-commkit
 cmake ../commkit -DFASTRTPS:STRING=/usr/local
 make -j6 VERBOSE=1
 
+# run tests
+./test/unit/commkit-tests
+
 # clang-format 3.8 is installed on OS X, which generates different formatting output
 # and we haven't updated to it locally yet. re-enable this once we've updated.
 if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then 
--- tools/travis/install-deps.sh
+++ tools/travis/install-deps.sh
@@ -6,10 +6,11 @@ cd ${TRAVIS_BUILD_DIR}/..
 curl -O https://capnproto.org/capnproto-c++-0.5.3.tar.gz
 tar zxf capnproto-c++-0.5.3.tar.gz
 cd capnproto-c++-0.5.3
-./configure --disable-dependency-tracking
+cmake -DBUILD_TESTING=OFF -DBUILD_TOOLS=OFF -DEXTERNAL_CAPNP=ON -DCMAKE_CXX_FLAGS=-fpic
 sudo make -j6 install
 
 # fast rtps
+cd ..
 git clone https://github.com/eProsima/Fast-RTPS.git
 mkdir build-Fast-RTPS && cd build-Fast-RTPS
 cmake ../Fast-RTPS
