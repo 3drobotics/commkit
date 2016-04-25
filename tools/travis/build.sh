f746273193a4cda7423fb344c6d39cdba7bd9bff
Liam Staskawicz <liam@stask.net>
2016-04-25 09:55:00 -0700

    gtest: don't run tests on OS X on Travis for now

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -10,7 +10,12 @@ cmake ../commkit -DFASTRTPS:STRING=/usr/local
 make -j6 VERBOSE=1
 
 # run tests
-./test/unit/commkit-tests
+# test commkit::Node::init() currently fails with "Function not implemented" on Travis.
+# have not identified what is throwing that, so disable until we do.
+# tests run OK on my OS X machine.
+if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then 
+    ./test/unit/commkit-tests
+fi
 
 # clang-format 3.8 is installed on OS X, which generates different formatting output
 # and we haven't updated to it locally yet. re-enable this once we've updated.
